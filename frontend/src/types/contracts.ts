3d74bd283b1b6fd7ea905c7a7d4d86275a71e282
llins <llinsomoudu@gmail.com>
2025-12-09 13:08:03 +0100

    types

--- frontend/src/types/contracts.ts
+++ frontend/src/types/contracts.ts
@@ -20,3 +20,15 @@ export type ContractConfig = {
   BaseNFT: BaseNFTContract;
   BaseStaking: BaseStakingContract;
 };
+
+export interface ContractAddresses {
+  BaseToken: `0x${string}`;
+  BaseNFT: `0x${string}`;
+  BaseStaking: `0x${string}`;
+  BalanceManager: `0x${string}`;
+  BalanceTracker: `0x${string}`;
+  BaseDEX: `0x${string}`;
+  BaseMarketplace: `0x${string}`;
+  BaseVesting: `0x${string}`;
+  BaseGovernance: `0x${string}`;
+}
