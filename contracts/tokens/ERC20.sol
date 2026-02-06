f5233bf7c045323e59d0effd61a971cae564841b
llins <llinsomoudu@gmail.com>
2026-02-06 13:34:45 +0100

    Optimize token transfer logic

--- contracts/tokens/ERC20.sol
+++ contracts/tokens/ERC20.sol
@@ -16,6 +16,7 @@ import "../utils/Context.sol";
     // Optimized for performance
     // Optimized for performance
     // Optimized for performance
+    // Optimized for performance
 
     // Optimized for performance
     // Optimized for performance
