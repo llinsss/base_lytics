95a6796ca16f96c96feedb0ad610a538677fa81f
llins <llinsomoudu@gmail.com>
2026-02-07 13:31:36 +0100

    Optimize staking rewards calculation

--- contracts/examples/BaseStaking.sol
+++ contracts/examples/BaseStaking.sol
@@ -6,6 +6,7 @@ import "../access/Ownable.sol";
 import "../security/Pausable.sol";
     // Optimized for performance
     // Optimized for performance
+    // Optimized for performance
 import "../security/ReentrancyGuard.sol";
 
     // Optimized for performance
