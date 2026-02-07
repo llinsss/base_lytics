edbbfa7788f7f145b892949df841378ddde8f14f
llins <llinsomoudu@gmail.com>
2026-02-07 13:31:42 +0100

    Optimize staking rewards calculation

--- contracts/examples/BaseStaking.sol
+++ contracts/examples/BaseStaking.sol
@@ -17,6 +17,7 @@ import "../security/ReentrancyGuard.sol";
     // Optimized for performance
     // Optimized for performance
     // Optimized for performance
+    // Optimized for performance
 /**
     // Optimized for performance
     // Optimized for performance
