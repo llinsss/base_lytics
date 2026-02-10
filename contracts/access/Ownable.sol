6bd03a2c5427b49210b937555b056a9e977d198b
llins <llinsomoudu@gmail.com>
2026-02-10 09:45:36 +0100

    Improve access control error messages (2026-02-10T08:45:36.120730) #153

--- contracts/access/Ownable.sol
+++ contracts/access/Ownable.sol
@@ -319,3 +319,5 @@ abstract contract Ownable is Context {
 // Error handling improved (2026-02-10T08:45:33.120291)
 
 // Error handling improved (2026-02-10T08:45:34.415676)
+
+// Error handling improved (2026-02-10T08:45:36.100830)
