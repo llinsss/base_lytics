5b8c2baa29733de4e44da6b7752ce3491f94cd34
llins <llinsomoudu@gmail.com>
2026-02-10 09:45:56 +0100

    Optimize token transfer logic (2026-02-10T08:45:56.653249) #411

--- contracts/tokens/ERC20.sol
+++ contracts/tokens/ERC20.sol
@@ -174,6 +174,7 @@ contract ERC20 is Context, IERC20, IERC20Metadata {
     }
 
     // Optimized for performance (2026-02-08T19:33:54.414242)
+    // Optimized for performance (2026-02-10T08:45:56.636691)
     /**
      * @dev See {IERC20-balanceOf}.
      */
