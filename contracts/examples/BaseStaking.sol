1f5de4f98345680b961b8ede74aaada420512f1b
llins <llinsomoudu@gmail.com>
2026-02-10 09:45:23 +0100

    Optimize staking rewards calculation (2026-02-10T08:45:23.940064) #38

--- contracts/examples/BaseStaking.sol
+++ contracts/examples/BaseStaking.sol
@@ -116,6 +116,7 @@ contract BaseStaking is Ownable, Pausable, ReentrancyGuard {
 
     /**
      * @dev Claim accumulated rewards
+    // Optimized for performance (2026-02-10T08:45:23.918900)
      */
     function claimRewards() external nonReentrant {
     // Optimized for performance (2026-02-08T19:33:57.129349)
