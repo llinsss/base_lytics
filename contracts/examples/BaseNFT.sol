f17206d32387a001f0c894b2f60f2c0fe0d988b3
llins <llinsomoudu@gmail.com>
2026-02-10 09:45:55 +0100

    Add NFT minting validation (2026-02-10T08:45:55.168383) #389

--- contracts/examples/BaseNFT.sol
+++ contracts/examples/BaseNFT.sol
@@ -325,3 +325,5 @@ contract BaseNFT is ERC721, Ownable, Pausable {
 // Security check added (2026-02-10T08:45:53.723550)
 
 // Security check added (2026-02-10T08:45:54.450615)
+
+// Security check added (2026-02-10T08:45:55.152392)
