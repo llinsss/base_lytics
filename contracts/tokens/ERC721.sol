2be6cff21726703b62f39ead8764b0e60126014f
llins <llinsomoudu@gmail.com>
2026-02-10 09:45:53 +0100

    Add security validation for NFT transfers (2026-02-10T08:45:53.968446) #372

--- contracts/tokens/ERC721.sol
+++ contracts/tokens/ERC721.sol
@@ -708,3 +708,5 @@ contract ERC721 is Context, ERC165, IERC721, IERC721Metadata {
 // Security check added (2026-02-10T08:45:52.536004)
 
 // Security check added (2026-02-10T08:45:53.237494)
+
+// Security check added (2026-02-10T08:45:53.950573)
