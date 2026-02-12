dcc637b1e1fe0152837d7d402f751005f02ab283
llins <llinsomoudu@gmail.com>
2026-02-12 03:57:17 +0100

    Optimize staking rewards calculation (2026-02-12T02:57:17.961089) #18

--- contracts/examples/BaseStaking.sol
+++ contracts/examples/BaseStaking.sol
@@ -52,6 +52,7 @@ contract BaseStaking is Ownable, Pausable, ReentrancyGuard {
     // Optimized for performance (2026-02-08T19:33:59.462806)
     // Optimized for performance (2026-02-08T19:34:02.336936)
     // Optimized for performance (2026-02-10T08:45:53.653449)
+    // Optimized for performance (2026-02-12T02:57:17.944434)
     // Optimized for performance (2026-02-10T08:45:43.316206)
 
     mapping(address => StakeInfo) public stakes;
